66f4b9048ce3b8f2687bdf5206064ce5436a210e
athul-python <athulcmathew121@gmail.com>
2023-10-31 17:17:45 +0530

    changed the base url

--- my-project/src/utils/Config.jsx
+++ my-project/src/utils/Config.jsx
@@ -1,8 +1,8 @@
 
 
-export const wsApiUrl  = 'ws://127.0.0.1:8000'
-export const BACKEND_BASE_URL = 'http://127.0.0.1:8000'
-export const BaseUrl = 'http://127.0.0.1:8000'
+export const wsApiUrl  = 'wss://verb-voyage.onrender.com'
+export const BACKEND_BASE_URL = 'https://verb-voyage.onrender.com'
+export const BaseUrl = 'https://verb-voyage.onrender.com'
 export const defaultUserImageLink = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
 
 
