b2dd2e7a53cfb86e219234dea4c450de144ef5d2
athul-python <athulcmathew121@gmail.com>
2023-11-02 21:20:22 +0530

    remove checkbox

--- my-project/src/components/pages/registration/Login2.jsx
+++ my-project/src/components/pages/registration/Login2.jsx
@@ -100,15 +100,7 @@ function Login() {
                   </div>
                   <div className="flex items-center justify-between">
                     <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input
-                          id="remember"
-                          aria-describedby="remember"
-                          type="checkbox"
-                          className="w-4 h-4 border border-cyan-400 rounded bg-cyan-400 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                          required
-                        />
-                      </div>
+                      
                       <div className="ml-3 text-sm">
                         <label htmlFor="remember" className="text-cyan-400 dark:text-cyan-400">Remember me</label>
                       </div>
