2153b0b2287f3e84f9bed1fa6ec94de2442ffedb
athul-python <athulcmathew121@gmail.com>
2023-11-02 21:17:48 +0530

    added verceljson file and user toogle button

--- my-project/src/components/pages/userside/UserHome.jsx
+++ my-project/src/components/pages/userside/UserHome.jsx
@@ -101,7 +101,7 @@ function UserHome() {
       }
     }
   }, []);
-
+const [toggle,setToggle]= useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleToggleMenu = () => {
@@ -131,6 +131,7 @@ function UserHome() {
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-solid-bg"
             aria-expanded="false"
+            onClick={()=>setToggle(!toggle)}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -149,6 +150,40 @@ function UserHome() {
               />
             </svg>
           </button>
+          {toggle && (
+          <div className="md:hidden w-full block" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-cyan bg-black rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <Link to="/profile">
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-cyan-400 md:dark:hover-text-blue-500 dark:hover-bg-gray-700 dark:hover-text-cyan-400 md:dark:hover-bg-transparent"
+                  >
+                    Profile
+                  </a>
+                </li>
+              </Link>
+              <li>
+                <a
+                  href="#"
+                  onClick={handleLogout}
+                  className="block py-2 pl-3 pr-4 text-cyan-400 rounded hover-bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark-text-cyan-400 md:dark-hover-text-blue-500 dark-hover-bg-gray-700 dark-hover-text-cyan-400 md:dark-hover-bg-transparent"
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+          )}
           <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
             <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
               <li>
