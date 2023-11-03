76509aa35e7c2db49e0e4f721fb86128ffd0e76a
athul-python <athulcmathew121@gmail.com>
2023-11-03 17:16:00 +0530

    changed to previous

--- my-project/src/components/pages/userside/CourseList.jsx
+++ my-project/src/components/pages/userside/CourseList.jsx
@@ -1,4 +1,7 @@
+// CourseList.js
 import React, { useState, useEffect } from 'react';
+import premium from '../../../assets/premiumbg.png';
+import free from '../../../assets/free.jpg';
 import PlaylistVideos from './PlaylistVideos';
 import Loader from '../Loader';
 import { BACKEND_BASE_URL } from '../../../utils/Config';
@@ -30,8 +33,8 @@ const CourseList = () => {
   };
 
   return (
-    <div className="bg-cyberpunk-bg text-cyberpunk-text min-h-screen p-8">
-      <h2 className="text-4xl font-bold text-cyberpunk-title mb-8">Top Courses</h2>
+    <div className="container mx-auto mt-8 bg-yellow-400 p-8 rounded-lg">
+      <h2 className="text-4xl font-bold text-black mb-8">Top Courses</h2>
 
       {isLoading ? (
         <Loader />
@@ -40,7 +43,7 @@ const CourseList = () => {
           {courses.map((course) => (
             <div
               key={course.id}
-              className={`bg-cyberpunk-bg text-cyberpunk-text p-6 rounded-lg shadow-md cursor-pointer transition duration-300 ${
+              className={`bg-white p-6 rounded-lg shadow-md cursor-pointer transition duration-300 ${
                 isEnrolled && selectedPlaylistId !== course.id ? 'opacity-50' : ''
               }`}
               onClick={() => handleEnroll(course.id)}
@@ -58,7 +61,7 @@ const CourseList = () => {
                   alt={'free'}
                 />
               )}
-              <h3 className="text-lg text-cyberpunk-title font-semibold mb-2">{course.title}</h3>
+              <h3 className="text-lg text-black font-semibold mb-2">{course.title}</h3>
               <div className="flex justify-between items-center">
                 {course.premium ? (
                   <span className="text-sm font-bold text-yellow-500">Premium</span>
